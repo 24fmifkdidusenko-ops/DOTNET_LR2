1e3b56e0e0a7d77149fdd1c4d5af797352efb526
24fmifkdidusenko-ops <24fmif.k.didusenko@std.udu.edu.ua>
2025-11-11 03:43:28 +0200

    Add files via upload

---
+++ lr2_VM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestNetStandard;
+
+class Program
+{
+    static void Main()
+    {
+        string email = "example@test.com";
+
+        Validation.Validate(email,
+            Success: () => Console.WriteLine("Email +!"),
+            Fail: () => Console.WriteLine("Email -!"));
+
+        Console.WriteLine("\nPress...");
+        Console.ReadKey();
+    }
+}
